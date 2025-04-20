64a9e10d36db6352b34439ec41b556f8f5eecd6a
Prisha Vohra <152265967+prishavohra@users.noreply.github.com>
2025-04-20 20:19:08 +0530

    Update Room.java

--- Room.java
+++ Room.java
@@ -1,1 +1,7 @@
+public class Room {
+    private int roomNumber;
+    private boolean isOccupied;
 
+    public Room(int roomNumber) {
+        this.roomNumber = roomNumber;
+        this.isOccupied = false;
