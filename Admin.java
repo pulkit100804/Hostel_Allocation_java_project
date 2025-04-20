c5d14aab544e39ae13b355c6402eea926316f86f
Rithikaa Redde <141546241+rithikaaredde@users.noreply.github.com>
2025-04-20 21:48:57 +0530

    initialized admin

--- Admin.java
+++ Admin.java
@@ -1,1 +1,17 @@
 
+import java.util.Scanner;
+
+public class Admin {
+    public static void adminMenu() {
+        Hostel hostel = new Hostel();
+        Scanner scanner = new Scanner(System.in);
+        boolean running = true;
+
+        while (running) {
+            System.out.println("\n🛠 Admin Panel 🛠");
+            System.out.println("1. Allocate Room");
+            System.out.println("2. Show Room Allocations");
+            System.out.println("3. Logout");
+            System.out.print("Choose an option: ");
+            int choice = scanner.nextInt();
+            scanner.nextLine();
