9fc26025eb25d441dc07c1c1a138d01afba30bca
Aneeka25 <154527297+Aneeka25@users.noreply.github.com>
2025-04-20 15:56:46 +0530

    Adding constructor for initializing username, password, and role

--- User.java
+++ User.java
@@ -2,3 +2,10 @@ public abstract class User {
     protected String username;
     protected String password;
     protected String role;
+
+    public User(String username, String password, String role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
