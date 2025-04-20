e62ab07ba85b3c3c8babac274910328088d2c26f
Prisha Vohra <152265967+prishavohra@users.noreply.github.com>
2025-04-20 20:14:15 +0530

    Update DBConnection.java

--- DBConnection.java
+++ DBConnection.java
@@ -1,1 +1,14 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/hostel_management";
+    private static final String JDBC_USER = "root";
+    private static final String JDBC_PASSWORD = "Archie@25";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD);
+    }
+}
 
