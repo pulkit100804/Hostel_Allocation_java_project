4e1373a5011aca58174311fa42ea016aa266bd94
Prisha Vohra <152265967+prishavohra@users.noreply.github.com>
2025-04-20 20:20:02 +0530

    Add more functionalities to Room.java

--- Room.java
+++ Room.java
@@ -5,3 +5,13 @@ public class Room {
     public Room(int roomNumber) {
         this.roomNumber = roomNumber;
         this.isOccupied = false;
+    }
+
+    public boolean isAvailable() {
+        return !isOccupied;
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+}
